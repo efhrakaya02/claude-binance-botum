7fce76bc51a5f97ed804708a52800d083a9be622
rakaya <efh.rakaya02@gmail.com>
2026-09-10 09:41:03 +0300

    Update __init__.py

--- trading_bot/execution/__init__.py
+++ trading_bot/execution/__init__.py
@@ -1,4 +1,5 @@
 from .binance_client import BinanceFuturesTradingClient
+from .dry_run_execution_engine import DryRunExecutionEngine
 from .execution_engine import ExecutionEngine
 
-__all__ = ["BinanceFuturesTradingClient", "ExecutionEngine"]
+__all__ = ["BinanceFuturesTradingClient", "ExecutionEngine", "DryRunExecutionEngine"]
